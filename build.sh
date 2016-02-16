2fd791f5f9a59d435763f4e0cd19410cbb5cb0fc
Hugues Bruant <hugues@aerofs.com>
2016-02-15 19:39:43 -0500

    Update img_fresh for compat with docker < 1.8

--- build.sh
+++ build.sh
@@ -10,22 +10,7 @@
 
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# $1 image
-# $2 source path
-# return true if the image is newer than the newest file in the source tree
-function img_fresh() {
-    # find creation date of container image, if present
-    # NB: remove sub-second precision and make it find-friendly
-    local created=$(
-        docker inspect --format='{{.Created}}' --type=image "$1" |\
-        cut -d. -f1 |\
-        sed 's/T/ /'
-    )
-    echo "$1 created: $created" 1>&2
-    # find first file in build context newer than image, if any
-    # NB: docker gives UTC timestamps, make sure find does not compare it to local time
-    [[ -n "$created" ]] && [[ -e "$2" ]] && [[ -z "$(TZ=UTC find "$2" -newermt "$created" | head -n 1)" ]]
-}
+source "$THIS_DIR/img_fresh.sh"
 
 IMAGE=nginx:alpine
 
